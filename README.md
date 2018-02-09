4379fd67e942f66dbd9dfeb2d1c6c011cfbeffaf
John Paul Ada <johnpaulada@Elie-Mini.local.gap>
2018-02-09 11:33:35 +0800

    edited link

--- README.md
+++ README.md
@@ -142,7 +142,7 @@ machine.addSubscriber((state, data) => {
 
 ## Graphics Credits
 
-Logo by my sister Jade Ada ([@paleportraits](https://www.instagram.com/blondiewiththebadhair/)).
+Logo by my sister Jade Ada ([@blondiewiththebadhair](https://www.instagram.com/blondiewiththebadhair/)).
 
 ### Other Icons
 <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
--- website/core/Footer.js
+++ website/core/Footer.js
@@ -56,7 +56,7 @@ class Footer extends React.Component {
               Star
             </a>
           </div>
-          <div>Logo by my sister Jade Ada <a href="https://www.instagram.com/blondiewiththebadhair/">@paleportraits</a>.</div>
+          <div>Logo by <a href="https://www.instagram.com/blondiewiththebadhair/">@blondiewiththebadhair</a>.</div>
           <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
         </section>
       </footer>
