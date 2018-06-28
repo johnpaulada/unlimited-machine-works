b634b885ac84d728d90d7f9c31b5af42e3817c62
John Paul Ada <johnpaulada@Johns-Mac-mini.local>
2018-06-28 08:49:30 +0800

    Better exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,24 @@
-import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'index.js',
-    format: 'umd',
-    name: 'UMW'
-  },
+  output: [
+    {
+      file: 'index.js',
+      format: 'cjs'
+    },
+    {
+      file: 'index.es.js',
+      format: 'es'
+    },
+    {
+      file: 'umw.min.js',
+      format: 'iife',
+      name: 'UMW'
+    },
+  ],
   plugins: [
-    resolve(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     }),
